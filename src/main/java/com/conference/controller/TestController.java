e62ca32ea646d1fb98225d513ddc40bab5e537a5
Lan-ce-lot <1984737645@qq.com>
2020-11-30 12:03:12 +0800

    添加了一个NCU的icon

---
+++ src/main/java/com/conference/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.conference.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+//    @GetMapping("/test")
+    @RequestMapping("/test")
+    public String hello() {
+        return "test";
+    }
+}
