73c6af47b606845ad6814ca52c7e6aafc83be2f4
Lan-ce-lot <1984737645@qq.com>
2020-12-05 20:56:33 +0800

    service完成测试

--- src/test/java/com/conference/ServiceTest.java
+++ src/test/java/com/conference/ServiceTest.java
@@ -1,12 +1,11 @@
 package com.conference;
 
-import com.conference.dao.DriverDao;
 import com.conference.dao.FleetDao;
-import com.conference.dao.PickUpDao;
-import com.conference.service.PickUpService;
-import com.conference.service.impl.DriverServiceImpl;
-import com.conference.service.impl.FleetServiceImpl;
-import com.conference.service.impl.PickUpServiceImpl;
+import com.conference.entity.Fleet;
+import com.conference.service.FleetService;
+import com.conference.service.imp.DriverServiceImp;
+import com.conference.service.imp.FleetServiceImp;
+import com.conference.service.imp.PickUpServiceImp;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,45 +18,29 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @SpringBootTest
 public class ServiceTest {
-
-//    @Autowired()
-
     @Autowired
-    private FleetServiceImpl fleetServiceImpl;
+    private FleetServiceImp fleetServiceImp;
     @Autowired
-    private DriverServiceImpl driverServiceImpl;
+    private DriverServiceImp driverServiceImp;
 
     @Autowired
-    DriverDao driverDao;
-    @Autowired
-    private PickUpService pickUpService;
+    private PickUpServiceImp pickUpServiceImp;
     @Autowired
     private FleetDao fleetDao;
 
-    @Autowired
-    private PickUpDao pickUpDao;
     @Test
     void contextLoads() {
 
 
 //        System.out.println(fleetDao.findAllFleet());
-        System.out.println(fleetServiceImpl.findAllFleet());
+        System.out.println(fleetServiceImp.findAllFleet());
     }
     @Test
     void fleetTest() {
-//        System.out.println(fleetDao.findFleetById(1));
-        System.out.println(fleetDao.updateFleet(1, "hhh", "123456", "19914665733"));
-
-        System.out.println(pickUpDao.findPickUpById(1));
-//        System.out.println(pickUpService.findPickUpById(1));
-//        Driver driver = new Driver(4, "hsc", "888888", null, "123456", "19914665732", false);
-//        System.out.println(driverServiceImp.addDriver(driver));
-//        System.out.println(driverDao.addDriver("hsc", "888888", null, "123456", "19914665732"));
-//        System.out.println(driverServiceImp.findDriverByPhone("12121212"));
-//        System.out.println(fleetServiceImp.findAllFleet());
-//        System.out.println(fleetServiceImp.findFleetById(1));
-//        System.out.println(fleetDao.deleteFleetById(1));
-////        System.out.println(fleetServiceImp.addFleet());
+        System.out.println(fleetServiceImp.findAllFleet());
+        System.out.println(fleetServiceImp.findFleetById(1));
+        System.out.println(fleetDao.deleteFleetById(1));
+//        System.out.println(fleetServiceImp.addFleet());
 //        System.out.println(fleetServiceImp.deleteFleetById(1));
 
     }
