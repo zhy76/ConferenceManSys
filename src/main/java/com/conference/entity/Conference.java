3d81050551dac543feebcd453beab6e88b3df725
zhy76 <958474674@qq.com>
2020-12-11 10:10:07 +0800

    更新conference和organizer实体类

--- src/main/java/com/conference/entity/Conference.java
+++ src/main/java/com/conference/entity/Conference.java
@@ -1,10 +1,14 @@
 package com.conference.entity;
 
-
+/**
+ * @Description 会议类
+ * @Author 左海余
+ * @Date 2020/12/10 21:46
+ * @sno 6109118041
+ */
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.sql.Timestamp;
-
 public class Conference {
     private Integer conferenceId;
     private Integer organizerId;
@@ -18,5 +22,90 @@ public class Conference {
     private String conferenceLocation; //会议地址
     private String conferenceInfo; //会议相关信息
 
+    @Override
+    public String toString() {
+        return "Conference{" +
+                "conferenceId=" + conferenceId +
+                ", organizerId=" + organizerId +
+                ", fleetId=" + fleetId +
+                ", hotelId=" + hotelId +
+                ", conferenceName='" + conferenceName + '\'' +
+                ", conferenceStart=" + conferenceStart +
+                ", conferenceEnd=" + conferenceEnd +
+                ", conferenceLocation='" + conferenceLocation + '\'' +
+                ", conferenceInfo='" + conferenceInfo + '\'' +
+                '}';
+    }
 
+    public Integer getConferenceId() {
+        return conferenceId;
+    }
+
+    public void setConferenceId(Integer conferenceId) {
+        this.conferenceId = conferenceId;
+    }
+
+    public Integer getOrganizerId() {
+        return organizerId;
+    }
+
+    public void setOrganizerId(Integer organizerId) {
+        this.organizerId = organizerId;
+    }
+
+    public Integer getFleetId() {
+        return fleetId;
+    }
+
+    public void setFleetId(Integer fleetId) {
+        this.fleetId = fleetId;
+    }
+
+    public Integer getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Integer hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getConferenceName() {
+        return conferenceName;
+    }
+
+    public void setConferenceName(String conferenceName) {
+        this.conferenceName = conferenceName;
+    }
+
+    public Timestamp getConferenceStart() {
+        return conferenceStart;
+    }
+
+    public void setConferenceStart(Timestamp conferenceStart) {
+        this.conferenceStart = conferenceStart;
+    }
+
+    public Timestamp getConferenceEnd() {
+        return conferenceEnd;
+    }
+
+    public void setConferenceEnd(Timestamp conferenceEnd) {
+        this.conferenceEnd = conferenceEnd;
+    }
+
+    public String getConferenceLocation() {
+        return conferenceLocation;
+    }
+
+    public void setConferenceLocation(String conferenceLocation) {
+        this.conferenceLocation = conferenceLocation;
+    }
+
+    public String getConferenceInfo() {
+        return conferenceInfo;
+    }
+
+    public void setConferenceInfo(String conferenceInfo) {
+        this.conferenceInfo = conferenceInfo;
+    }
 }
