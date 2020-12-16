0e72ceb00d1dcaeb36d311df815abd0e87c550dd
Lan-ce-lot <1984737645@qq.com>
2020-12-16 13:43:29 +0800

    dev合并glz Merge branch 'lingchen' of https://github.com/zhy76/ConferenceManSys into dev
    # Conflicts:
    #	.gitignore
    #	mvnw
    #	pom.xml
    #	src/main/java/com/conference/ConferenceApplication.java
    #	src/main/java/com/conference/controller/ParticipantController.java
    #	src/main/java/com/conference/service/TokenService.java
    #	src/main/java/com/conference/util/result/Result.java
    #	src/main/java/com/conference/util/result/ResultCode.java
    #	src/main/resources/application.properties
    #	src/main/resources/static/index.html
    #	src/main/resources/static/popupsignin.html
    #	src/main/resources/static/popupsignup.html
    #	src/main/resources/static/司机个人信息.html
    #	src/main/resources/static/司机待接送.html
    #	src/main/resources/static/注册.html
    #	src/main/resources/static/登录.html
    #	src/main/resources/static/车队信息.html
    #	src/test/java/com/conference/ConferenceApplicationTests.java

---
+++ src/main/java/com/conference/service/Impl/ConferenceServiceImpl.java
@@ -0,0 +1,51 @@
+package com.conference.service.impl;
+
+import com.conference.dao.ConferenceDao;
+import com.conference.entity.Conference;
+import com.conference.service.ConferenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author 谢 娇
+ * @Date 2020/12/5 22:31
+ * @sno 6109118015
+ */
+@Service("conferenceService")
+public class ConferenceServiceImpl implements ConferenceService {
+
+    @Override
+    public int addConference(Conference conference) {
+        return conferenceDao.addConference(conference);
+    }
+
+    @Override
+    public Conference queryConferenceById(Integer conferenceId) {
+        return conferenceDao.queryConferenceById(conferenceId);
+    }
+    @Autowired
+    private ConferenceDao conferenceDao;
+
+    @Override
+    public List<Conference> queryConferences() {
+        return conferenceDao.queryConferences();
+    }
+
+    @Override
+    public int updateConference(Conference conference) {
+        return conferenceDao.updateConference(conference);
+    }
+
+    @Override
+    public int deleteConference(Integer conferenceId) {
+        return conferenceDao.deleteConference(conferenceId);
+    }
+
+    @Override
+    public Conference queryConferenceByConferenceId(Integer conferenceId) {
+        return conferenceDao.queryConferenceByConferenceId(conferenceId);
+    }
+}
