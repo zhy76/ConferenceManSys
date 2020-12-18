bd5baf52a47f2ee84f74b1a9fcacc4adb1569cf1
Lan-ce-lot <1984737645@qq.com>
2020-12-18 18:45:41 +0800

    merge lingchen&zhy

--- src/main/java/com/conference/controller/OrganizerController.java
+++ src/main/java/com/conference/controller/OrganizerController.java
@@ -2,7 +2,6 @@ package com.conference.controller;
 
 import com.conference.entity.Organizer;
 import com.conference.service.OrganizerService;
-<<<<<<< HEAD
 import com.conference.service.TokenService;
 import com.conference.util.result.Result;
 import com.conference.util.result.ResultCode;
@@ -168,34 +167,12 @@ public class OrganizerController {
         Organizer getOrganizerInfo = organizerService.findOrganizerById((Integer) claims.get("organizerId"));
         return Result.success("getOrganizerInfo", getOrganizerInfo);
     }
-=======
-import com.conference.util.result.Result;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @Description
- * @Author 谢 娇
- * @Date 2020/12/18 10:45
- * @sno 6109118015
- */
-@RestController
-@RequestMapping("/organizer")
-public class OrganizerController {
-
-    @Autowired
-    private OrganizerService organizerService;
 
 
-    @GetMapping("/getOrganizerInfo")
+    @GetMapping("/getOrganizerInfoById")
     public Result getOrganizerInfo(@RequestParam Integer organizerId) {
-        Organizer getOrganizerInfo = organizerService.findOrganizerById(organizerId);
-        System.out.println(getOrganizerInfo);
-        return Result.success("getOrganizerInfo", getOrganizerInfo);
+        Organizer getOrganizerInfoById = organizerService.findOrganizerById(organizerId);
+        System.out.println(getOrganizerInfoById);
+        return Result.success("getOrganizerInfoById", getOrganizerInfoById);
     }
-
->>>>>>> lingchen
 }
