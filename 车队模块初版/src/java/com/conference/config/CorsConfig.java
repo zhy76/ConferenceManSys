41ac0ffee663d5a3676af458832387c248b9723b
hardworkingcj <1254582307@qq.com>
2020-12-22 16:17:46 +0800

    bug修复

--- 车队模块初版/src/java/com/conference/config/CorsConfig.java
+++ 车队模块初版/src/java/com/conference/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.conference.config;
+﻿package com.conference.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
