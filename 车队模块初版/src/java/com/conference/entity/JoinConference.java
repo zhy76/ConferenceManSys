00373622cce19c396ed0f9b198c26596193e80f9
hardworkingcj <1254582307@qq.com>
2020-12-22 16:27:04 +0800

    车队接送

---
+++ 车队模块初版/src/java/com/conference/entity/JoinConference.java
@@ -0,0 +1,94 @@
+package com.conference.entity;
+
+
+import java.sql.Timestamp;
+
+/**
+ * @Description
+ * @Author 谢 娇
+ * @Date 2020/12/5 17:48
+ * @sno 6109118015
+ */
+
+public class JoinConference {
+    public Integer getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(Integer participantId) {
+        this.participantId = participantId;
+    }
+
+    public Integer getConferenceId() {
+        return conferenceId;
+    }
+
+    public void setConferenceId(Integer conferenceId) {
+        this.conferenceId = conferenceId;
+    }
+
+    public Boolean getIsPutup() {
+        return isPutup;
+    }
+
+    public void setIsPutup(Boolean isPutup) {
+        this.isPutup = isPutup;
+    }
+
+    public Boolean getPickup() {
+        return isPickup;
+    }
+
+    public JoinConference() {
+    }
+
+    @Override
+    public String toString() {
+        return "JoinConference{" +
+                "participantId=" + participantId +
+                ", conferenceId=" + conferenceId +
+                ", isPutup=" + isPutup +
+                ", isPickup=" + isPickup +
+                ", toTime='" + toTime + '\'' +
+                ", returnTime='" + returnTime + '\'' +
+                ", trainNumber='" + trainNumber + '\'' +
+                '}';
+    }
+
+    public void setPickup(Boolean pickup) {
+        isPickup = pickup;
+    }
+
+    public String getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(String toTime) {
+        this.toTime = toTime;
+    }
+
+    public String getReturnTime() {
+        return returnTime;
+    }
+
+    public void setReturnTime(String returnTime) {
+        this.returnTime = returnTime;
+    }
+
+    public String getTrainNumber() {
+        return trainNumber;
+    }
+
+    public void setTrainNumber(String trainNumber) {
+        this.trainNumber = trainNumber;
+    }
+
+    private Integer participantId;
+    private Integer conferenceId;
+//    private Integer isPutup; //是否需要住宿
+    private Boolean isPutup;
+    private Boolean isPickup; //是否需要接送
+    private String toTime; //参会往时间
+    private String returnTime; //参会返时间
+    private String trainNumber; //车次或航班号
+}
