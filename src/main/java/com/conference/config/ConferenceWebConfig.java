8146df2d1ae61d4a1e230124755eac15cb102ed1
lcy <842654846@qq.com>
2020-12-23 09:23:58 +0800

    拦截器配置

--- src/main/java/com/conference/config/ConferenceWebConfig.java
+++ src/main/java/com/conference/config/ConferenceWebConfig.java
@@ -1,6 +1,8 @@
 package com.conference.config;
 
 import com.conference.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,12 +10,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @Author: liuCenYu
  * @Date: 2020/12/23 1:05
  **/
+
+@Configuration
 public class ConferenceWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**") //所有的路径都拦截
-                .excludePathPatterns("/登录New","/注册New","/admin/**") ; //除了主页和CSS JS 等非页面文件
+                .excludePathPatterns(
+                        "/login.html",
+                        "/注册New.html",
+                        "/**.js",
+                        "/**.css") ;
+        //除了主页和CSS JS 等非页面文件
     }
 }
--- src/main/java/com/conference/interceptor/LoginInterceptor.java
+++ src/main/java/com/conference/interceptor/LoginInterceptor.java
@@ -5,6 +5,7 @@ import com.conference.service.TokenService;
 import com.conference.service.impl.TokenServiceImpl;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,7 @@ import java.util.Date;
  * @Author: liuCenYu
  * @Date: 2020/12/22 22:51
  **/
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Autowired
@@ -22,21 +24,37 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 处理OPTIONS请求(预检请求)，具体看参阅“参考链接>>>http的协议的跨域cors 和 options请求的一些理解”
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "86400");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Content-Type", "text/html;charset=utf-8");
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return false;
+        }
         //登录检查逻辑,ajax请求头添加携带token操作！
         String token = request.getHeader("token");
-        System.out.println(token);
-        token = token == null ? "" : token ;
-        Claims claims = tokenService.parseToken(token);
-        Date preDate = (Date)claims.get("timeExpiration");
-        Date curDate = new Date();
-        Long expire = (curDate.getTime() - preDate.getTime())/1000 - TokenServiceImpl.expiration;//计算token的剩余存活时间
-        if( expire > 0 ) //如果还存活，说明是登录状态就直接放行
-        {
-            //放行
-            return true;
+        token = token == null ? "" : token;
+        try {
+            Claims claims = tokenService.parseToken(token);
+            Date preDate = (Date) claims.get("timeExpiration");
+            Date curDate = new Date();
+            Long expire = (curDate.getTime() - preDate.getTime()) / 1000 - TokenServiceImpl.expiration;//计算token的剩余存活时间
+            if (expire > 0) //如果还存活，说明是登录状态就直接放行
+               return true;   //放行
+
+        } catch (Exception e) {
+            //否则，就请求转发到登录页面
+            response.setHeader("Content-type", "text/html;charset=UTF-8");
+            response.setCharacterEncoding("utf-8");
+            response.setContentType("text/html;charset=UTF-8");
+            String url = "/login.html";
+            response.sendRedirect(url);
+            return false;
         }
-        //否则，就请求转发到登录页面
-        request.getRequestDispatcher("/登录New.html").forward(request,response);
         return false;
     }
 }
